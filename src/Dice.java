f41a0e6753d1c3067d5e5a47271494bb8dcef21e
davmusko <davmusko@gmail.com>
2018-01-26 00:21:03 -0500

    Added comments and reformatted

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Dice {
@@ -6,12 +5,14 @@ public class Dice {
 		Scanner scan = new Scanner(System.in);
 		String rerun;
 		int rollNum = 1;
+		
+//Prompts user to input N-sides to set to dice
 		System.out.println("Welcome to the Grand Circus Casino!");
 		System.out.println("How many sides should the die have?");
 		int sides = scan.nextInt();
 		scan.nextLine();
 		
-		
+//Generates two random numbers 1-Nsides and prints them
 		do {
 			System.out.printf("Roll %d:\n",rollNum);
 			int result1 = DiceRollerApp.rollDie(sides);
@@ -19,6 +20,7 @@ public class Dice {
 			System.out.println(result1);
 			System.out.println(result2);
 			
+//Checks for snakeeyes, boxcars, or craps then calls given method
 			if(result1 == 1 && result2 == 1) {
 				DiceRollerApp.snakeEyesReturner();
 			}
@@ -29,12 +31,12 @@ public class Dice {
 				DiceRollerApp.boxCarReturner();
 			}
 			
+//Prompts user to roll again, if 'y' is inputed, reroll and add 1 to roll counter
 			System.out.println("Would you like to roll again?");
 			rerun = scan.nextLine();
 			rollNum++;
-				
 		}while (rerun.equalsIgnoreCase("y"));
-		
+	scan.close();
 	}
 	
 	
--- src/DiceRollerApp.java
+++ src/DiceRollerApp.java
@@ -2,15 +2,25 @@ import java.util.Random;
 
 public class DiceRollerApp {
 	
+//Returns and prints String Craps if dice sum = 7 or 11
 	public static void crapsReturner() {
 		System.out.println("Craps!");
 	}
+
+//Returns and prints String Snake-Eyes if dice roll 1 & 1
 	public static void snakeEyesReturner() {
 		System.out.println("Snake-Eyes!");
 	}
+	
+//Returns and prints String Box Cars if dice roll 6 & 6
 	public static void boxCarReturner() {
 		System.out.println("Box Cars!");
 	}
+	
+/* Returns a random number from 1-Nsides 
+ * Set seed to 0 for snake eyes
+ * Set seed to 5 for boxcars
+ */
 	public static int rollDie(int sides) {
 		Random ranNum = new Random();
 		int rollResult = ranNum.nextInt(sides) +1;
